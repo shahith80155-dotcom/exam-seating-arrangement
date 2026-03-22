9b49e68aca4a2a9e98994373204117aae40a1aec
Shahith S <shahith80155@gmail.com>
2026-03-22 12:18:21 +0530

    updated server for deployment

--- server.js
+++ server.js
@@ -572,4 +572,8 @@ app.delete("/reset-all", (req, res) => {
         })
     })
 })
-app.listen(3000, ()=> console.log("Server running"))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT)
+})
