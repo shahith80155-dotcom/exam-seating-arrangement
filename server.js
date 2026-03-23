799a89ac7b3892b0fb9c1e0167b7059c1fc6d57d
Shahith S <shahith80155@gmail.com>
2026-03-23 10:05:46 +0530

    fiww

--- server.js
+++ server.js
@@ -75,9 +75,9 @@ db.connect(err => {
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-}
+        user: "shahith80155@gmail.com",
+        pass: "bmjnchknztjjnrre"
+    }
 })
 
 let otpStore = {}
